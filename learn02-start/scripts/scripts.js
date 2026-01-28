8a94d6e3bee3cb34f4574f9f792e3fc317840c17
David <dsalazar@students.mchenry.edu>
2026-01-27 22:14:44 -0600

    Assignment 2

---
+++ learn02-start/scripts/scripts.js
@@ -0,0 +1,10 @@
+function myfunction2() {
+  document.getElementById("sample").innerHTML =
+    "Changed from a function in an external script";
+}
+
+// Single line comment
+
+/* 
+block line comment
+*/
